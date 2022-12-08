035068bc30330fac2724dd024d2159c8c7d031c6
Ali Hameed <106717840+alihmeed@users.noreply.github.com>
2022-12-08 20:29:48 +0200

    Add files via upload

---
+++ day-2100/main.py
@@ -0,0 +1,20 @@
+print("Getting to know you!")
+print()
+name = input("What's your name? ")
+print("Nice to meet you", name + "!")
+print()
+favorite_food = input("What's your favorite food? ")
+print(favorite_food + "!", "you got me hungry!")
+print()
+favorite_music = input("What's your favorite musical genre? ")
+print(favorite_music + ",", "is so cool!")
+print()
+place = input("And lastly, where do you live? ")
+print(place + "?", "I'd like to go there someday!")
+print()
+print("Thanks for sharing this with me", name + "!",
+      "I'll never forget that...")
+print()
+print("Your name is", name + ",", "your favorite food is", favorite_food + ",",
+      "your favorite musical genre is", favorite_music + ",",
+      "and you live in ", place + "!")
