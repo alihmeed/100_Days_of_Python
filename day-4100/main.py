14e282d065e79354f8fde201067b7b5fbb30b207
Ali Hameed <106717840+alihmeed@users.noreply.github.com>
2022-12-10 14:56:11 +0200

    Add files via upload

---
+++ day-4100/main.py
@@ -0,0 +1,47 @@
+print(
+    "\033[31m", '''
+█▀▄ █ ▄▀█ █▄▄ █░░ █▀█   █▀ █ █▀▄▀█ █░█ █░░ ▄▀█ ▀█▀ █▀█ █▀█
+█▄▀ █ █▀█ █▄█ █▄▄ █▄█   ▄█ █ █░▀░█ █▄█ █▄▄ █▀█ ░█░ █▄█ █▀▄
+''', "\033[0m")
+print()
+print(
+    '''
+The dream continues to haunt me old friend. It always starts with a
+journey to a distant land. There I find a city in flames.
+
+Streets choked with corpses. Unthinkable destruction.
+I witness senseless slaughter. Brother against brother.
+
+Pure hatred. And then, executions. And then suffering surrounded me
+until my turn comes. It burned my eyes. Break my bones. I wake in terror.
+
+There's no-one left to stand against them. You think me mad, old friend.
+But I know these dreams. They tell of the future.
+
+Hell is coming, brother.'''
+    "\033[31m", "Hell", "\033[0m"
+    "is coming.")
+print()
+print("Tyrael: Wake up", "\033[33m"
+      "nephalim"
+      "\033[0m"
+      "..")
+print()
+name = input("Tyrael: What's your name? ")
+print()
+print("Tyrael:"
+      "\033[33m", name, "\033[0m"
+      "there isn't much time left, this is our final stand.")
+print()
+print("Tyrael:"
+      "\033[33m", name, "\033[0m"
+      "you are the ..", "\033[35m"
+      "dark wanderer"
+      "\033[0m")
+print()
+print("Tyrael: We must stop "
+      "\033[31m"
+      "Lilith"
+      "\033[0m", "before she destroys Sanctuary..")
+print()
+print(".. to be continued")
